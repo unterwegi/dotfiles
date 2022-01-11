852aee4e74f9fce18cb0c90d102d8438164969fa
Ingo Unterweger <unterweger.external@infineon.com>
2022-01-11 09:12:11 +0100

    add bootstrap script to fetch nerd fonts

---
+++ .bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+font_dir="$HOME/.local/share/fonts"
+
+mkdir -p "$font_dir"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CascadiaCode.zip -P "$font_dir"
+
+cd "$font_dir" || exit
+
+unzip CascadiaCode.zip
+rm CascadiaCode.zip
