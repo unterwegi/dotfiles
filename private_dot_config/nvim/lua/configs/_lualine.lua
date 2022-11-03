45286e5f55b0d7ea10de3610746c50ea1b9eb4e0
Ingo Unterweger <ingo.unterwegi@gmail.com>
2022-11-03 16:28:46 +0100

    nvim: add lualine

---
+++ private_dot_config/nvim/lua/configs/_lualine.lua
@@ -0,0 +1,7 @@
+require("lualine").setup {
+    options = {
+        globalstatus = true,
+        icons_enabled = true,
+        theme = "nightfox",
+    },
+}
--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -1,17 +1,17 @@
 local fn = vim.fn
 local cmd = vim.cmd
 
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  _G.packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  _G.packer_bootstrap = fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
 end
 
-local packer = require('packer')
+local packer = require("packer")
 
 packer.init({
   display = {
     open_fn = function()
-      return require('packer.util').float({ border = 'single' })
+      return require("packer.util").float({ border = "single" })
     end
   },
   profile = {
@@ -26,6 +26,13 @@ packer.startup({
 
         -- Theme: colour scheme
         use "EdenEast/nightfox.nvim"
+        use {
+            "nvim-lualine/lualine.nvim",
+            requires = { "kyazdani42/nvim-web-devicons" },
+            config = function()
+                require("configs._lualine")
+            end
+        }
 
         -- Automatically set up your configuration after cloning packer.nvim
         if packer_bootstrap then
