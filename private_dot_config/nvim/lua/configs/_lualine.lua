2dd7b62b1c0be35178fd855b869ccb5e2babd0d8
Ingo Unterweger <ingo.unterwegi@gmail.com>
2022-11-09 21:50:24 +0100

    customized lualine + enabled tabline

--- private_dot_config/nvim/lua/configs/_lualine.lua
+++ private_dot_config/nvim/lua/configs/_lualine.lua
@@ -3,5 +3,51 @@ require("lualine").setup {
         globalstatus = true,
         icons_enabled = true,
         theme = "nightfox",
+        section_separators = { left = "", right = ""},
+    },
+    sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = {
+            {
+                "diff",
+                symbols = {
+                    added = " ",
+                    modified = " ",
+                    removed = " ",
+                },
+            },
+            {
+                "diagnostics",
+                sources = {
+                    "nvim_diagnostic"
+                }
+            },
+            {
+                "filename",
+                path = 1,
+                shorting_target = 30,
+                symbols = {
+                    modified = " ",
+                    readonly = " ",
+                    unnamed = " ",
+                }
+            }
+        },
+        lualine_x = { "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" }
+    },
+    tabline = {
+        lualine_b = {
+            {
+                "buffers",
+                mode=4,
+                buffers_color = {
+                    active = "lualine_b_normal",
+                    inactive = "lualine_c_inactive",
+                },
+            }
+        },
     },
 }
