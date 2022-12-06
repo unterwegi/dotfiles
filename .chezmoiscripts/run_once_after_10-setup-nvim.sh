77e5312223477f5e673814f2eb4c305315041572
Ingo Unterweger <ingo.unterwegi@gmail.com>
2022-12-06 15:21:23 +0100

    final fix in nvim bootstrap script

--- .chezmoiscripts/run_once_after_10-setup-nvim.sh
+++ .chezmoiscripts/run_once_after_10-setup-nvim.sh
@@ -3,6 +3,8 @@
 set -eufo pipefail
 
 echo -e "\033[0;32m>>>>> Begin Setting Up Neovim <<<<<\033[0m"
-git clone --depth 1 https://github.com/wbthomason/packer.nvim  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+packer_dir="$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
+mkdir -p "$packer_dir"
+git clone --depth 1 https://github.com/wbthomason/packer.nvim "$packer_dir"
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 echo -e "\033[0;32m>>>>> Finish Setting Up Neovim <<<<<\033[0m"
