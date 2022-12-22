696e309669303358021b8aa556198bd18049a6a6
Ingo Unterweger <ingo.unterwegi@gmail.com>
2022-12-22 10:56:08 +0100

    fish: switch to tide as prompt

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,9 +1,3 @@
-set -U fish_greeting
-
-fish_add_path -a -P $HOME/bin $HOME/.local/bin
-# aqua managed binaries should be used first
-fish_add_path -P $HOME/.local/share/aquaproj-aqua/bin
-
 # set up my EDITOR and VISUAL depending on whats available
 if type -q nvim
   set -gx EDITOR nvim
@@ -18,11 +12,6 @@ end
 
 # Tell aqua to use my config inside my home
 set -gx AQUA_GLOBAL_CONFIG $HOME/.config/aqua.yaml
-
-# Launch Starship prompt
-if type -q starship
-  starship init fish | source
-end
 
 # Launch direnv
 if type -q direnv
