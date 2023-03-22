9113de2d4ec809e3e08a45fd44367dcd6c1107b5
Ingo Unterweger <ingo.unterwegi@gmail.com>
2023-03-22 19:36:45 +0100

    cfg: nvim: add gnikdroy/projections.nvim for project session handling

---
+++ private_dot_config/nvim/lua/plugins/projects.lua
@@ -0,0 +1,30 @@
+return {
+    {
+        "gnikdroy/projections.nvim",
+        config = function()
+            require("projections").setup({
+                workspaces = {
+                    "~/projects",
+                    "~/repos",
+                }
+            })
+
+            require('telescope').load_extension('projections')
+            vim.keymap.set("n", "<leader>fp", function() vim.cmd("Telescope projections") end)
+
+            -- Autostore session on VimExit
+            local Session = require("projections.session")
+            vim.api.nvim_create_autocmd({ 'VimLeavePre' }, {
+                callback = function() Session.store(vim.loop.cwd()) end,
+            })
+
+            -- Switch to project if vim was started in a project dir
+            local switcher = require("projections.switcher")
+            vim.api.nvim_create_autocmd({ "VimEnter" }, {
+                callback = function()
+                    if vim.fn.argc() == 0 then switcher.switch(vim.loop.cwd()) end
+                end,
+            })
+        end
+    }
+}
