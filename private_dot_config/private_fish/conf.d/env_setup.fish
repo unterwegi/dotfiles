10e5b9f5f099d7c9cc4975cb702cd75982250d4b
Ingo Unterweger <ingo.unterwegi@gmail.com>
2023-07-26 08:07:02 +0200

    feat(shell): automatic DOCKER_HOST and GDK_BACKEND setup

---
+++ private_dot_config/private_fish/conf.d/env_setup.fish
@@ -0,0 +1,13 @@
+if command -q docker
+    # if there is a rootless docker socket for the current user, use it
+    set -l docker_socket "/run/user/$(id -u)/docker.sock"
+    if ! test -e "$docker_socket"
+        # otherwise default to the normal docker socket location
+        set docker_socket "/var/run/docker.sock"
+    end
+
+    set -gx DOCKER_HOST "unix://$docker_socket"
+end
+
+# prefer wayland over x11 as GDK backend (helps some java applications under wayland when they use GTK)
+set -gx GDK_BACKEND "wayland,x11"
