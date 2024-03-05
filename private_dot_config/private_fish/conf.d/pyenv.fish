61957f37574d22a00150eca4c7a080e1f435f6c8
Ingo Unterweger <ingo.unterwegi@gmail.com>
2024-03-05 08:13:21 +0100

    feat(fish): integrate better with installed pyenv

--- private_dot_config/private_fish/conf.d/pyenv.fish
+++ private_dot_config/private_fish/conf.d/pyenv.fish
@@ -1,8 +1,6 @@
-if command -q pyenv
-    set -gx PYENV_ROOT $HOME/.pyenv
-    fish_add_path -g $PYENV_ROOT/bin
+set -gx PYENV_ROOT $HOME/.pyenv
+test -d $PYENV_ROOT/bin && fish_add_path -g $PYENV_ROOT/bin
 
-    if status is-interactive
-        pyenv init - | source
-    end
+if command -q pyenv && status is-interactive
+    pyenv init - | source
 end
