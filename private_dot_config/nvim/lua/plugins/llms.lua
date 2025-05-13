cbbcdd675d3af894be5305b9cfa370318c497cab
Ingo Unterweger <ingo.unterwegi@gmail.com>
2025-05-13 14:59:53 +0200

    feat(nvim): add olimorris/codecompanion.nvim with enabled copilot

---
+++ private_dot_config/nvim/lua/plugins/llms.lua
@@ -0,0 +1,26 @@
+return {
+    {
+        "olimorris/codecompanion.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+            "github/copilot.vim",
+        },
+        config = function()
+            require("codecompanion").setup({
+                strategies = {
+                    chat = {
+                        adapter = "copilot",
+                    },
+                    inline = {
+                        adapter = "copilot",
+                    },
+                    cmd = {
+                        adapter = "copilot",
+                    }
+                },
+            })
+            vim.g.copilot_enabled = false
+        end,
+    },
+}
